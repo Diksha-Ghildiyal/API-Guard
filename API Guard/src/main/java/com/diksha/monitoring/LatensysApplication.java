6777bd4a47a4c1144b196b6a92571412437e5e7c
Diksha-Ghildiyal <dikshaghildiyal2011@gmail.com>
2026-09-09 23:14:08 +0530

    Add files via upload

---
+++ API Guard/src/main/java/com/diksha/monitoring/LatensysApplication.java
@@ -0,0 +1,13 @@
+package com.harshit.monitoring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LatensysApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LatensysApplication.class, args);
+    }
+
+}
